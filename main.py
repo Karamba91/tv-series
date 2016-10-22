2de85635e9402ae28ae571c927297ba184d7ee5f
Karamba91 <erikhenriksson91@gmail.com>
2016-10-22 23:49:15 +0200

    Improving html decoding \n Decoder removes all unnecessary HTML code, todo: improve re matching, creating Episode object, store in a good way.

--- main.py
+++ main.py
@@ -2,11 +2,30 @@ import re,fetch_web,Episode
 from Episode_HTMLParser import Episode_HTMLParser
 
 def list_episodes():
-    return fetch_web.fetch_html()
+    text_file=open("HTML.txt","r")
+    r=text_file.read()
+    text_file.close()
+    return r
+    #return fetch_web.fetch_html()
 
 def decode_html(html):
+    debug_i=0          # Debug index
     parser = Episode_HTMLParser()
     parser.feed(html)
     print parser.data
+    for i in parser.data:
+        # To indicate which iteration loop fails
+        debug_i = debug_inc(debug_i)
+        # Remove uneccesary information from string. Split at beginning of info.
+        pure=re.split("(\d+\..+)",i)[1]
+        print pure
+        # Identifying information from string.
+        m = re.match("(?P<total_episode>^\d+)(\.)(\s+)(?P<season>\d+)(\-)(?P<episode>\d+)(\s+)(?P<date>[\d\s\w]+)",pure)
+        print m.groupdict()
 
+def debug_inc(index):
+    index += 1
+    print index
+    return index 
+# decode_html(list_episodes())
 # <pre>Test snippet</pre>
