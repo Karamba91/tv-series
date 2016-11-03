9ec50c628716ae186c00e637f9a5c79c93d367b9
Karamba91 <erikhenriksson91@gmail.com>
2016-11-03 20:05:28 +0100

    Interpret html, print all episodes with broadcast date and episode name

--- Episode.py
+++ Episode.py
@@ -1,14 +1,19 @@
+import datetime
 class Episode:
-    def __init__(self,n_name="",n_season=0,n_episode=0):
+    def __init__(self,n_name="",n_season=0,n_episode=0,date='2000-01-01'):
         self.name = n_name
         self.season = n_season
         self.episode = n_episode
+        self.broadcast_date = date
 
-        def get_name(self):
-            return self.name
+    def get_name(self):
+        return self.name
 
-        def get_season(self):
-            return self.season
+    def get_season(self):
+        return self.season
 
-        def get_episode(self):
-            return self.episode
+    def get_episode(self):
+        return self.episode
+
+    def get_brdcstDate(self):
+        return 
--- main.py
+++ main.py
@@ -1,31 +1,56 @@
-import re,fetch_web,Episode
+import re,fetch_web,datetime
 from Episode_HTMLParser import Episode_HTMLParser
+from Episode import Episode
 
 def list_episodes():
-    text_file=open("HTML.txt","r")
-    r=text_file.read()
+    text_file = open("HTML.txt","r")
+    r = text_file.read()
     text_file.close()
     return r
     #return fetch_web.fetch_html()
 
 def decode_html(html):
-    debug_i=0          # Debug index
+    debug_i=0  # Debug index
     parser = Episode_HTMLParser()
     parser.feed(html)
-    print parser.data
     for i in parser.data:
         # To indicate which iteration loop fails
-        debug_i = debug_inc(debug_i)
+        # debug_i = debug_inc(debug_i)
         # Remove uneccesary information from string. Split at beginning of info.
-        pure=re.split("(\d+\..+)",i)[1]
-        print pure
-        # Identifying information from string.
-        m = re.match("(?P<total_episode>^\d+)(\.)(\s+)(?P<season>\d+)(\-)(?P<episode>\d+)(\s+)(?P<date>[\d\s\w]+)",pure)
-        print m.groupdict()
+        pure = re.split("(\d+\..+)",i)
+        if len(pure) > 2:
+            finish_flag = 0
+            pure = pure[1]
+            # Identifying information from string.
+            m = re.match("(?P<total_episode>^\d+)(\.)(\s+)(?P<season>\d+)(\-)(?P<episode>\d+)(\s+)(?P<date>[\d\s\w]+)",pure)
+            episode_dict = m.groupdict()
+        elif finish_flag == 0:
+            finish_flag = 1
+            name = pure[0]
+            new_episode = Episode(name,episode_dict['season'],episode_dict['episode'])
+            print "Episode", new_episode.get_episode(),"in season", \
+            new_episode.get_episode(), "is named", new_episode.get_name()
 
 def debug_inc(index):
     index += 1
     print index
     return index 
-# decode_html(list_episodes())
+
+def interpret_date(dateStr):
+    months=['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+    monthDict=dict(zip(months,range(1,13)))
+    dd = dateStr.split()
+    day = dd[0]
+    month = monthDict[dd[1]]
+    if len(dd[2]) == 2:
+        year = "20" + dd[2]
+    elif len(dd[2]) == 4:
+        year = dd[2]
+    return datetime.date(year,month,day)
+
+
+
+
+
+decode_html(list_episodes())
 # <pre>Test snippet</pre>
